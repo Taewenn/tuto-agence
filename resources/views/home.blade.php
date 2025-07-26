0a732ac3206391d649720d25b4229824edab5f0a
Ugo <ugo@orangebleu.be>
2025-07-26 15:22:57 +0200

    chapitre : composants blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('base')
 
 @section('content')
+    <x-alert type="danger" class="mb-4">
+        Des informations
+    </x-alert>
+
     <div class="px-4 py-5 my-5 text-center">
         <h1 class="display-5 fw-bold text-body-emphasis">Bienvenue sur MonAgence</h1>
         <div class="col-lg-6 mx-auto">
