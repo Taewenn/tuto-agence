7bbd64558d80fa226489f5370dfc6d24cc33d51d
Ugo <ugo@orangebleu.be>
2025-07-26 14:48:06 +0200

    chapitre : accesseurs & mutateurs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,8 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $properties = Property::orderBy('created_at', 'desc')->limit(4)->get();
+        $properties = Property::available()->recent()->limit(4)->get();
+        dd($properties->first());
         return view('home', ['properties' => $properties]);
     }
 }
--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Property extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'title',
         'description',
@@ -30,5 +34,15 @@ class Property extends Model
     public function getSlug(): string
     {
         return Str::slug($this->title);
+    }
+
+    public function scopeAvailable(Builder $builder, bool $available = true): Builder
+    {
+        return $builder->where('sold', !$available);
+    }
+
+    public function scopeRecent(Builder $builder): Builder
+    {
+        return $builder->orderBy('created_at', 'desc');
     }
 }
